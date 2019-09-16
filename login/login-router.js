f962e26d881295143d7e77352a6bc8db5f914a56
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-16 14:24:11 -0600

    working on login-router

--- login/login-router.js
+++ login/login-router.js
@@ -1,8 +1,29 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
 
 const Login = require('./login-model')
 
 const router = express.Router()
+
+router.get('/users', (req, res) => {
+    Login.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({ error: 'Could not load users'})
+        })
+})
+
+router.post('/register', (req, res) => {
+    let { username, password } = req.body;
+
+    const hash = bcrypt.hashSync(password, 10)
+
+    users.createUser({ username, password: hash })
+
+})
 
 
 module.exports = router
